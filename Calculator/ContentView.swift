fa1b1867e2d79c12e592532cafa375e284606c77
Arpit Kohli <arpit.kohli@nagarro.com>
2020-05-24 21:41:23 +0530

    Initial Commit

---
+++ Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Arpit Kohli on 24/05/20.
+//  Copyright © 2020 Arpit Kohli. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
